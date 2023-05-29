a93d6eb149e2fe3e851b4b4b8c5751d8be2da0a0
sb shuvon <sbshuvon07@gmail.com>
2023-05-29 09:54:22 +0600

    optimize

---
+++ optimize.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+
+
+
+    return 0;
+}
+
